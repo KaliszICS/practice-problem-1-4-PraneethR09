d87564e8bb9a4861519a219640ea931dcad3c6f5
PraneethR09 <praneethrameswaran9@gmail.com>
2024-10-30 18:34:16 -0400

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -2,16 +2,24 @@
 
 def q1():
   #Write Assignment code here
-
+word = input("Input a word:")
+print(word)
 def q2():
   #Write Assignment code here
-
+word = input("Input your first name")
+print("Hello" + Word)
 def q3():
   #Write Assignment code here
-
+word1 = input("Input your first name")
+word2 = input("Input your last name")
+print(word2, end=" ")
+print(word1)
 def q4():
   #Write Assignment code here
-
+word1 = input("Input a student")
+word2 = input("Input another student")
+print(word2, end=" ")
+print(word1)
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
 
